3a254c292eb244b452f85d1a9f0ee18311d39e1e
plu-s <1947283246@qq.com>
2019-05-04 20:06:16 +0800

    丰富了账户设置页面的内容

--- app/src/main/java/com/corydon/miu/AccountSettingActivity.java
+++ app/src/main/java/com/corydon/miu/AccountSettingActivity.java
@@ -1,11 +1,13 @@
 package com.corydon.miu;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.corydon.miu.bean.User;
@@ -26,12 +28,27 @@ import okhttp3.Response;
 public class AccountSettingActivity extends AppCompatActivity {
     @BindView(R.id.nameInput)
     EditText nameInput;
-    @BindView(R.id.passwordsInput)
-    EditText passwordsInput;
+    @BindView(R.id.newPasswordsInput)
+    EditText newPasswordsInput;
     @BindView(R.id.submitButton)
     Button submitButton;
-    private Handler handler=new Handler();
-    User user=MainActivity.user;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.mailView)
+    TextView mailView;
+    @BindView(R.id.mailShow)
+    EditText mailShow;
+    @BindView(R.id.nameView)
+    TextView nameView;
+    @BindView(R.id.oldPasswordsView)
+    TextView oldPasswordsView;
+    @BindView(R.id.oldPasswordsInput)
+    EditText oldPasswordsInput;
+    @BindView(R.id.newPasswordsView)
+    TextView newPasswordsView;
+    private Handler handler = new Handler();
+    User user = MainActivity.user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,27 +57,30 @@ public class AccountSettingActivity extends AppCompatActivity {
         init();
     }
 
-    private void toast(String msg){
+    private void toast(String msg) {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private void init(){
+    private void init() {
+        toolbar.setTitle("账户设置");
+        setSupportActionBar(toolbar);
+        mailShow.setText(user.getMail());
         nameInput.setText(user.getName());
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String newName=nameInput.getText().toString();
-                String newPasswords=passwordsInput.getText().toString();
-                Map<String,String> forms=new HashMap<>();
-                forms.put("mail",user.getMail());
-                forms.put("name",newName);
-                forms.put("oldPasswords",user.getPasswords());
-                forms.put("newPasswords",newPasswords);
+                String newName = nameInput.getText().toString();
+                String newPasswords = newPasswordsInput.getText().toString();
+                Map<String, String> forms = new HashMap<>();
+                forms.put("mail", mailShow.getText().toString());
+                forms.put("name", newName);
+                forms.put("oldPasswords", oldPasswordsInput.getText().toString());
+                forms.put("newPasswords", newPasswords);
                 HttpUtil.doFormPost(Configures.URL_ACCOUNT_SETTING, forms, new Callback() {
                     @Override
                     public void onFailure(Call call, IOException e) {
@@ -69,23 +89,21 @@ public class AccountSettingActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(Call call, Response response) throws IOException {
-                        if(response.code()==200){
-                            try{
-                                JSONObject object=new JSONObject(response.body().string());
-                                int result=object.getInt("result");
-                                if(result==200){
+                        if (response.code() == 200) {
+                            try {
+                                JSONObject object = new JSONObject(response.body().string());
+                                int result = object.getInt("result");
+                                if (result == 200) {
                                     user.setName(newName);
                                     user.setPasswords(newPasswords);
                                     toast(object.getString("data"));
                                     finish();
-                                }
-                                else
+                                } else
                                     toast(object.getString("data"));
-                            }catch(JSONException e){
+                            } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                        }
-                        else{
+                        } else {
                             toast("请联系管理员");
                         }
                     }
